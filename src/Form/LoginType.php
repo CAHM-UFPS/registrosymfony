f61577bf6de05618a04087b9f84707b3d52eabe8
CAHM-UFPS <camilohdez1396@gmail.com>
2022-10-05 17:11:01 +0000

    More validations

--- src/Form/LoginType.php
+++ src/Form/LoginType.php
@@ -3,16 +3,27 @@
 namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class LoginType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('user')
-            ->add('password')
+            ->add('user', TextType::class, [
+                'constraints' => [
+                    new NotBlank
+                ]
+            ])
+            ->add('password', PasswordType::class, [
+                'constraints' => [
+                    new NotBlank
+                ]
+            ])
         ;
     }
 
